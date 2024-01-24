9ae6a96ce8679ee46bc7dc2e020d8179799adf9b
guinafelix <oguilhermeleitefelix@gmail.com>
2024-01-24 15:33:26 -0300

    feat: serving static files

--- server.py
+++ server.py
@@ -1,21 +1,25 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
 class RequestHandler(BaseHTTPRequestHandler):
     # ...page template...
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
-    
-    def create_page(self):
-        values = {
-        'date_time' : self.date_time_string(),
-        'client_host' : self.client_address[0],
-        'client_port' : self.client_address[1],
-        'command' : self.command,
-        'path' : self.path
-        }
-        page = self.Page.format(**values)
-        return page
+        try:
+            # Figure out what exactly is being requested.
+            full_path = os.getcwd() + self.path
+
+            # if it does not exist
+            if not os.path.isfile(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+        
+            # its a file
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+        except Exception as msg:
+            self.handle_error(msg)
         
     def send_page(self, page):
         self.send_response(200)
@@ -24,20 +28,36 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(page.encode())
     
-    Page = '''\
-    <html>
-    <body>
-    <table>
-    <tr> <td>Header</td> <td>Value</td> </tr>
-    <tr> <td>Date and time</td> <td>{date_time}</td> </tr>
-    <tr> <td>Client host</td> <td>{client_host}</td> </tr>
-    <tr> <td>Client port</td> <td>{client_port}s</td> </tr>
-    <tr> <td>Command</td> <td>{command}</td> </tr>
-    <tr> <td>Path</td> <td>{path}</td> </tr>
-    </table>
-    </body>
-    </html>
-    '''
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)         
+    
+    
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+        
+    # Send actual content.
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
 
 
 if __name__ == '__main__':
