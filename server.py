20cf8c2dbf4a1816773520b9993fb1a209b1e203
guinafelix <oguilhermeleitefelix@gmail.com>
2024-01-27 14:16:56 -0300

    feat: add cgi protocol

--- server.py
+++ server.py
@@ -1,6 +1,26 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 
+class base_case(object):
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+    
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+    
+    def test(self, handler):
+        assert False, 'Not implemented.'
+    
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+
 class case_cgi_file(object):
     '''Something runnable.'''
     def test(self, handler):
@@ -9,7 +29,7 @@ class case_cgi_file(object):
     def act(self, handler):
         handler.run_cgi(handler.full_path)
 
-class case_directory_index_file(object):
+class case_directory_index_file(base_case):
     '''Serve index.html page for a directory.'''
     def index_path(self, handler):
         return os.path.join(handler.full_path, 'index.html')
@@ -19,7 +39,7 @@ class case_directory_index_file(object):
             os.path.isfile(self.index_path(handler))
             
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(self.index_path(handler))
 
 class case_directory_no_index_file(object):
     '''Serve listing for a directory without an index.html page.'''
@@ -33,20 +53,20 @@ class case_directory_no_index_file(object):
     def act(self, handler):
         handler.list_dir(handler.full_path)
 
-class case_no_file(object):
+class case_no_file(base_case):
     '''File or directory does not exist.'''
     def test(self, handler):
             return not os.path.exists(handler.full_path)
     def act(self, handler):
-        raise ServerException("'{0}' not found".format(handler.path))
+        raise ServerException("'{0}' not found".format(handler.full_path))
 
-class case_existing_file(object):
+class case_existing_file(base_case):
     '''File exists.'''
     def test(self, handler):
         return os.path.isfile(handler.full_path)
     
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
 
 class case_always_fail(object):
@@ -60,8 +80,10 @@ class case_always_fail(object):
 
 class RequestHandler(BaseHTTPRequestHandler):
     Cases = [case_no_file(),
+        case_cgi_file(),
         case_existing_file(),
-        case_directory_index_file,
+        case_directory_index_file(),
+        case_directory_no_index_file(),
         case_always_fail()]
     
     def do_GET(self):
@@ -136,6 +158,14 @@ class RequestHandler(BaseHTTPRequestHandler):
         except OSError as msg:
             msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
             self.handle_error(msg)
+            
+    def run_cgi(self, full_path):
+        cmd = "python " + full_path
+        child_stdin, child_stdout = os.popen2(cmd)
+        child_stdin.close()
+        data = child_stdout.read()
+        child_stdout.close()
+        self.send_content(data)
 
 
 if __name__ == '__main__':
